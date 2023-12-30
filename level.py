3de772d99b122e47a15f8cca0291f377d0a13239
Reid-C <theophiluschikezie@gmail.com>
2023-12-30 09:10:44 +0100

    Update tile file

--- level.py
+++ level.py
@@ -1,5 +1,7 @@
 import pygame
 from settings import *
+from tile import Tile
+from player import Player
 
 class Level:
     def __init__(self):
@@ -12,11 +14,15 @@ class Level:
         self.obstacles_sprites = pygame.sprite.Group()
 
         # sprite setup
-        self.create_map
+        self.create_map()
 
     def create_map(self):
-        for row in WORLD_MAP:
-            print(row)
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites])
 
     def run(self):
         # update and draw the game
