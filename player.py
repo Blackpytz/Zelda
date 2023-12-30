e0de9da4efdbb4da0abfeaeb4d894a428d36f986
Reid-C <theophiluschikezie@gmail.com>
2023-12-30 18:56:49 +0100

    Add player collision

--- player.py
+++ player.py
@@ -2,12 +2,15 @@ import pygame
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.image.load('graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
 
         self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+        self.obstacle_sprites = obstacle_sprites
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -24,4 +27,38 @@ class Player(pygame.sprite.Sprite):
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
         else:
-            self.direction.y = 0
+            self.direction.x = 0
+
+    def move(self, speed):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        self.rect.x += self.direction.x * speed
+        self.colllision('horizontal')
+        self.rect.y += self.direction.y * speed
+        self.colllision('vertical')
+        # self.rect.center += self.direction * speed
+
+    def colllision(self, direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0: # moving left
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x <0: # moving right
+                        self.rect.left = sprite.rect.right
+
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0: # moving down
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0: # moving up
+                        self.rect.top = sprite.rect.bottom
+
+        if direction == 'vertical':
+            pass
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
