5890f8726d3dbeedea03ae3c23e96aa69bc75290
Reid-C <theophiluschikezie@gmail.com>
2024-01-01 09:48:49 +0100

    Add support file

---
+++ support.py
@@ -0,0 +1,11 @@
+from csv import reader
+
+def import_csv_layout(path):
+    terrain_map = []
+    with open(path) as level_map:
+        layout = reader(level_map, delimiter = ',')
+        for row in layout:
+            terrain_map.append(list(row))
+        return terrain_map
+    
+def import_folder(path)
