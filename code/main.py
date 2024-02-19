af91e689ba3f7b23ccf75055637b6475b050f634
Reid-C <theophiluschikezie@gmail.com>
2024-02-19 07:47:36 +0100

    Update Zelda

--- code/main.py
+++ code/main.py
@@ -1,5 +1,6 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 class Game:
 	def __init__(self):
@@ -7,9 +8,11 @@ class Game:
 		# general setup
 		pygame.init()
 		self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
-		pygame.display.set_caption('Zelda')
+		pygame.display.set_caption('ZELDA')
 		self.clock = pygame.time.Clock()
-	
+
+		self.level = Level()
+
 	def run(self):
 		while True:
 			for event in pygame.event.get():
@@ -18,6 +21,7 @@ class Game:
 					sys.exit()
 
 			self.screen.fill('black')
+			self.level.run()
 			pygame.display.update()
 			self.clock.tick(FPS)
 
